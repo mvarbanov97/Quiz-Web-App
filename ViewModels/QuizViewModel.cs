1bb6da2ed36f63fe98acae1f5ae90ef39fde749f
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-03-17 09:36:07 +0200

    Added Initial ViewModels for Quiz/Category/Score

---
+++ ViewModels/QuizViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using QuizWebApp.Models;
+using QuizWebApp.Models.API_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizWebApp.ViewModels
+{
+    public class QuizViewModel
+    {
+        public List<QuestionInfo> Questions { get; set; } = new List<QuestionInfo>();
+    }
+}
---
+++ ViewModels/ScoreViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizWebApp.ViewModels
+{
+    public class ScoreViewModel
+    {
+        public int CorrectAnswers { get; set; }
+
+        public int WrongAnswers { get; set; }
+    }
+}
