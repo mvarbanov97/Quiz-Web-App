437bd6df38a7d52d4174f120d18cbdaf63bc3f12
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-03-17 09:15:20 +0200

    Implemented ICategoryService Interface

---
+++ Services/Contracts/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using QuizWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizWebApp.Services.Contracts
+{
+    public interface ICategoryService : IService
+    {
+        ICollection<Category> GetAllCategoriesFromDb();
+    }
+}
