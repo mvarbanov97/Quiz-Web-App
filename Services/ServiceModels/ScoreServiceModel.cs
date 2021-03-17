daa08d38908bd1f34075c8f3d3e931e97915a6fc
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-03-17 11:41:26 +0200

    Created SelectListGenerator and ScoreServiceModel.cs

---
+++ Services/ServiceModels/ScoreServiceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizWebApp.Services.ServiceModels
+{
+    public class ScoreServiceModel
+    {
+        public int CorrectAnswersCount { get; set; }
+
+        public int WrongAnswersCount { get; set; }
+
+        public double Average { get; set; }
+    }
+}
