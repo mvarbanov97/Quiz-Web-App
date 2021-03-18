c50fe194cfd385b80334f5bf3b0da1dd724616d6
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-03-18 02:25:01 +0200

    Moved root of repository . ../ Renamed the Web project.

---
+++ src/QuizWebApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QuizWebApp.Data;
+using QuizWebApp.Models;
+using QuizWebApp.Web.Models;
+using System.Diagnostics;
+
+namespace QuizWebApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<User> userManager;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db, UserManager<User> userManager)
+        {
+            _logger = logger;
+            this.db = db;
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
